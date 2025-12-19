9a8b379f4b149f377a831fc8abdee3b1cb8d0867
Manjunath Yadgiri <2020056819@dkasc.ac.in>
2025-12-19 22:30:56 +0530

    Add frontend and backend structure for Online Auction System

---
+++ Online Auction System Backend/onlineBiddingSystem/src/test/java/com/onlineBiddingSystem/OnlineBiddingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineBiddingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBiddingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
